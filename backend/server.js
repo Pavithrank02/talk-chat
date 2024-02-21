ce4a42289e4e0e5693a735128125455f3968dd88
Pavithran2 <pavithrank02@gmail.com>
2024-02-21 20:24:47 +0530

    updated

--- backend/server.js
+++ backend/server.js
@@ -7,8 +7,8 @@ const userRoutes = require('./routes/userRoutes');
 const { notFound, errorHandler } = require("./middlewares/errorMiddleware");
 const chatRoutes = require("./routes/chatRoutes");
 const messageRoutes = require("./routes/messageRoutes")
+const path = require("path");
 const PORT = process.env.PORT || 5000
-
 dotenv.config()
 
 connectDb()
@@ -31,6 +31,23 @@ app.get('/api/chat/:id', (req, res) => {
 app.use('/api/user', userRoutes)
 app.use("/api/chat", chatRoutes);
 app.use("/api/message", messageRoutes);
+// --------------------------deployment------------------------------
+
+const __dirname1 = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1, "/frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
+}
+
+// --------------------------deployment------------------------------
 
 app.use(notFound)
 app.use(errorHandler)
